2108f0a7c6928439b0d10e3a930577b9367cbf12
Xie Yi <wez733@live.cn>
2017-05-03 22:57:35 +0800

    OSASK的鼠标驱动并没有问题
    vmware虚拟机里面跑qemu果然有坑

--- mouse.cpp
+++ mouse.cpp
@@ -1,10 +1,9 @@
 #include "asmfunc.hpp"
 #include "fifo.hpp"
 #include "inputdevices.hpp"
-#include "isr.hpp"
 
 FIFO *mousefifo;
-static int mousedata0;
+int mousedata0;
 void enable_mouse(FIFO *fifo,int data0,struct MOUSE_DEC *mdec){
 	//激活鼠标
 	mousefifo=fifo;
@@ -18,42 +17,31 @@ void enable_mouse(FIFO *fifo,int data0,struct MOUSE_DEC *mdec){
 	return;
 }
 
-int mouse_decode(struct MOUSE_DEC *mdec,uint8_t dat){
-	if(mdec->phase==0){
-		if(dat==0xfa)mdec->phase=1;
+int mouse_decode(struct MOUSE_DEC *mdec,unsigned char dat){
+	if(mdec->phase==-1){
+		if(dat==0xfa)
+			mdec->phase=0;
 		return 0;
 	}
-	if(mdec->phase==1){
-		if((dat&0xc8)==0x08){
-			mdec->buf[0]=dat;
-			mdec->phase=2;
-		}
+	mdec->buf[mdec->phase]=dat;
+	if(!mdec->phase && (dat&0xc8)!=0x08)
 		return 0;
-	}
-	if(mdec->phase==2){
-		mdec->buf[1]=dat;
-		mdec->phase=3;
-		return 0;
-	}
-	if(mdec->phase==3){
-		mdec->buf[2]=dat;
-		mdec->phase=1;
+	if(!(mdec->phase=(mdec->phase+1)%3)){
 		mdec->btn=mdec->buf[0]&0x07;
 		mdec->x=mdec->buf[1];
 		mdec->y=mdec->buf[2];
-		if((mdec->buf[0]&0x10)!=0)
+		if(mdec->buf[0]&0x10)
 			mdec->x|=0xffffff00;
-		if((mdec->buf[0]&0x20)!=0)
+		if(mdec->buf[0]&0x20)
 			mdec->y|=0xffffff00;
 		mdec->y=-mdec->y;
 		return 1;
 	}
-	return -1;
+	return 0;
 }
 
 //来自PS/2鼠标的中断
 void mouse_handler(registers_t regs){
-	(void)regs;
 	int data;
 	io_out8(PIC1_OCW2,0x64);
 	io_out8(PIC0_OCW2,0x62);
